eb747719f101dd2695b4888facf61ede9d0af208
sakomitov <sak7@rice.edu>
2020-05-15 12:40:25 -0500

    get rid of static site things for now

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,17 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
-// var StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin');
-// var locals = {
-//   routes: [
-//     '/',
-//   ]
-// };
-
 module.exports = {
     plugins: [
         new MiniCssExtractPlugin({
             filename: 'styles_bundle.css',
-        }),
-        // new StaticSiteGeneratorPlugin('main', locals.routes),
+        })
     ],
     entry: './src/index.tsx',
     devtool: 'inline-source-map',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        // libraryTarget: 'umd'
     },
     module: {
         rules: [
